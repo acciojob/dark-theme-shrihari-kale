797d55d8640c9f28587829670469220df4c36cb4
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2025-12-07 22:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+function swapTheme() {
+    const app = document.getElementById("app");
+    const btn = document.getElementById("swap");
+
+    // Toggle classes
+    if (app.classList.contains("day")) {
+        app.classList.remove("day");
+        app.classList.add("night");
+
+        btn.classList.remove("button_day");
+        btn.classList.add("button_night");
+
+    } else {
+        app.classList.remove("night");
+        app.classList.add("day");
+
+        btn.classList.remove("button_night");
+        btn.classList.add("button_day");
+    }
+}
